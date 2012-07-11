704b60d3f4c631d980300a37ee70db6f5ef2fa12
Alex-ZL <yesterdayzhang@gmail.com>
2012-07-12 00:12:17 +0800

    update MyGame, fight method and monster class

--- July/MyGame.py
+++ July/MyGame.py
@@ -51,6 +51,9 @@ class game(object):
 			role.strength = randint(15,19)
 			role.willpower = randint(1,5)
 			role.agility = 30 - role.strength - role.willpower
+			role.hp = 600
+			role.skill = "Double Strike"
+			role.sk_damage = 75
 			if role.strength == 19:
 				role.weapon = "harmmer"
 			else:
@@ -62,13 +65,19 @@ class game(object):
 			role.willpower = randint(2,6)
 			role.strength = 30 - role.agility - role.willpower
 			role.weapon = "bow"
+			role.hp = 500
+			role.skill = "Magic Arrow"
+			role.sk_damage = 70
 			print "Hit and Run, you're really fight master, my ancher."
 		else:
 			role.career = "Wizard"
 			role.willpower = randint(15,19)
 			role.strength = randint(1,5)
 			role.agility = 30 - role.willpower - role.strength
-			role.weapon = "Wund"
+			role.weapon = "Wand"
+			role.hp = 400
+			role.skill = "God's Thunder"
+			role.sk_damage = 85
 			print "Magic is everything, noble wizard!"
 
 		print "Now, start your advanture, my %s !" % role.career
@@ -82,11 +91,37 @@ class game(object):
 			mission = getattr(self,next_mission_name)
 			next_mission_name = mission(self.myrole)
 	
-	def fight(self, role):
-		role.attack()
+	def fight(self, role, monster):
+		""" define the figth process between role and monster,
+		    it won't be over until one side bleeds up his HP"""
+		role_damage = 0
+		monster_damage = 0
+		print "This a fight between %s and %s" % (role.career, monster.name)
+		raw_input("Press Enter to begin!!!")
+		while 1:
+			role_damage += role.attack()
+			#raw_input(">>>>>>")
+			monster_damage += monster.attack()
+			#raw_input(">>>>>>")
+
+			if role_damage >= monster.hp:
+				print "You Win! %s" % role.career
+				print "your total damage: ", role_damage
+				print "Monster total damage: ", monster_damage
+				break
+			
+			if monster_damage >= role.hp:
+				print "You lose this fight, %s" % role.career
+				print "your total damage: ", role_damage
+				print "Monster total damage: ", monster_damage
+				return 'death'
+
 
 
 	def valley(self, role):
+		""" to pass through the valley, you need to win a game against gaint,
+		    you could fight with him, or play dice with him, it depends on 
+			your choice."""
 		print "Here is valley, a gaint's manor"
 		print "Would you want to pass through this valley over the gaint's body?"
 		print "Or just try your luck, play dice with he?"
@@ -96,9 +131,8 @@ class game(object):
 			print "Only input 1 or 2 please!"
 			return 'valley'
 		if decide == 1:
-			print "just figth"
-			for x in xrange(10):
-				print role.attack()
+			gaint = monster('Gaint')
+			self.fight(role,gaint)
 		else:
 			if role.luck > 7:
 				print "Are you kidding me? Luck point %d" % role.luck
@@ -121,10 +155,16 @@ class game(object):
 		return 'jungle'
 
 	def jungle(self, role):
+		""" Dark jungle lives a evil wizard, if you could answer his randle,
+		    you could pass through the jungle safely, or you have to get 
+			through over this evil guy's body"""
 		print "In the dark jungle, there lives a evil wizard"
 		return 'castle'
 
 	def castle(self, role):
+		""" Powerful dragon is the last monster you need to face, there is no
+		    other way rather than fightting could pass through it, so prepare
+			yourself, and make the final fight """
 		print "Lo and behold, you get to the dragon's castle"
 		return 'death'
 
@@ -153,14 +193,9 @@ class role(object):
 		self.luck = randint(1,10)
 		self.career = properties["career"]
 		self.weapon = properties["weapon"]
-
-		if self.career == "Warrior":
-			self.hp = 1200
-		elif self.career == "Archer":
-			self.hp = 1000
-		else:
-			self.hp = 900
-	
+		self.hp = 500
+		self.skill = "Unknown"
+		self.sk_damage = 50
 
 	def attack(self):
 		"""attack method return damage value depending on career"""
@@ -168,43 +203,61 @@ class role(object):
 		if self.career == "Warrior":
 			damage = 1.5*self.strength + 1*self.agility + 0.5*self.willpower + randint(0, self.luck)
 		elif self.career == "Archer":
-			damage = 1.2*self.strength + 1.4*self.agility + 0.4*self.willpower + randint(0, self.luck)
+			damage = 1.0*self.strength + 1.6*self.agility + 0.4*self.willpower + randint(0, self.luck)
 		else:
 			damage = 0.4*self.strength + 0.9*self.agility + 1.7*self.willpower + randint(0, self.luck)
 
 		if randint(0, self.agility) == 0:
 			damage = 0
-			print "miss! make no damage"
+			print "Miss! You make no damage"
+		elif randint(1,5) == 3:
+			damage = self.sk_damage + randint(0,self.luck)
+			print "You launch you speciall skill: %s !" % self.skill
+			print "***That makes %d ponits damage, cool!" % damage
 		else:
-			print "You make %.1f points damage" % damage
+			print "You hit it and  make %.1f points damage" % damage
 		return damage
 
 class monster(object):
 	def __init__(self,name):
 		"""set the monster's properties"""
-		if name == "gaint":
-			self.hp = 1200
-			self.max_damage = 40
+		if name == "Gaint":
+			self.hp = 600
+			self.name = name
+			self.max_damage = 70
 			self.unique_skill = "Frenzy Strike"
 			self.us_damage = 80
-		elif name == "evil wizard":
-			self.hp = 800
+			self.hit_rate = 5
+		elif name == "Evil Wizard":
+			self.hp = 400
+			self.name = name
 			self.max_damage = 50
 			self.unique_skill = "Ice Blast"
 			self.us_damage = 120
-		elif name == "dragon":
-			self.hp = 1500
+			self.hit_rate = 8
+		elif name == "Dragon":
+			self.hp = 700
+			self.name = name
 			self.max_damage = 60
 			self.unique_skill = "True Dragon Fire"
 			self.us_damage = 200
+			self.hit_rate = 7
 	
 	def attack(self):
 		"""define the monster's attack, return the damage value"""
 		damage = 0
-
-
-
-
+		if randint(1,10) > self.hit_rate:
+			print "%s missed this attack, good luck!" % self.name
+		else:
+			if randint(1,6) == 3:
+				print "%s use his unique skill: %s !!!" % (self.name, self.unique_skill)
+				damage = self.us_damage - randint(0,20)
+				print "###That make %d points damage, damn it!" % (damage)
+			else:
+				damage = self.max_damage - randint(0,10)
+				print "%s hit you and make %d points damage" % (self.name,damage)
+			 
+		return damage
 
 
 MyGame = game()
