4d15598f10c61241bc0b508536357e657e0b008d
Alex-ZL <yesterdayzhang@gmail.com>
2012-08-26 16:31:10 +0800

    igonre pyc file

---
+++ August/simple_unittest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import unittest
+
+class DefaultTestCase(unittest.TestCase):
+	def setUp(self):
+		self.what = 'ever'
+
+	def tearDown(self):
+		self.what = ''
+	
+	def testrun1(self):
+		self.assert_(self.what == 'foo', 'incorrect default size')
+
+	def testrun2(self):
+		self.assert_(self.what == 'ever', 'incorrect answer 2')
+
+
+unittest.main()
